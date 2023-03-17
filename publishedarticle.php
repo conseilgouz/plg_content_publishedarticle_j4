94798a72dce54f420b6c8f21c4273389fdd94649
pmleconte <pascal.leconte@conseilgouz.com>
2023-03-17 14:44:36 +0100

    2.0.1
    Subject/Body parameters using special tags

--- publishedarticle.php
+++ publishedarticle.php
@@ -8,16 +8,19 @@
 
 defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Table\Table;
-use Joomla\CMS\Access\Access;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\Component\Content\Site\Model\ArticleModel; 
 use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\String\PunycodeHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
 use Joomla\CMS\Plugin\CMSPlugin;
 
 class PlgContentPublishedArticle extends CMSPlugin
 {
+	protected $itemtags, $info_cat, $tag_img,$cat_img, $url, $needCatImg,$needIntroImg;
+	
 	public function onContentAfterSave($context, $article, $isNew)
 	{
 
@@ -62,14 +65,15 @@ class PlgContentPublishedArticle extends CMSPlugin
 		}
 		// parametres du plugin
 		$categories = $this->params->get('categories', array());
-		$usergroups = $this->params->get('usergroups', '');		
-		$msgcreator = $this->params->get('msgcreator', '');		
+		$usergroups = $this->params->get('usergroups', array());
+		$msgcreator = $this->params->get('msgcreator', 0);
+		
 		$db = Factory::getDbo();
 		$query = $db->getQuery(true)
-			->select($db->quoteName('id'))
-			->from($db->quoteName('#__users'))
-		// 	->where($db->quoteName('sendEmail') . ' = 1')
-			->where($db->quoteName('block') . ' = 0');
+			->select($db->quoteName('u.id'))
+			->from($db->quoteName('#__users').' as u ')
+			->join('LEFT',$db->quoteName('#__user_usergroup_map').' as g on u.id = g.user_id')
+			->where($db->quoteName('block') . ' = 0 AND g.group_id IN ('.implode(',',$usergroups).')');
 		$db->setQuery($query);
 		$users = (array) $db->loadColumn();
 
@@ -86,7 +90,6 @@ class PlgContentPublishedArticle extends CMSPlugin
 			$model->setState('list.limit', 1);
 			$model->setState('filter.published', 1);
 			$model->setState('filter.featured', 'show');
-			$model->setState('filter.category_id', array());
 			// Access filter
 			$access = ComponentHelper::getParams('com_content')->get('show_noauth');
 			$model->setState('filter.access', $access);
@@ -98,21 +101,36 @@ class PlgContentPublishedArticle extends CMSPlugin
 			$article = $model->getItem($articleid);
 			if (!in_array($article->catid,$categories)) continue; // wrong category
 			$creatorId = $article->created_by;
+			if (!in_array($creatorId,$users)) { // creator not in users array : add it
+			    $users[] = $creatorId;
+			}
 			$creator = Factory::getUser($creatorId);
-			$url = "<a href='".URI::root()."index.php?option=com_content&view=article&id=".$articleid."' target='_blank'> en cliquant sur ce lien</a>"; 
+			$this->url = "<a href='".URI::root()."index.php?option=com_content&view=article&id=".$articleid."' target='_blank'> en cliquant sur ce lien</a>"; 
+			$this->info_cat = $this->getCategoryName($article->catid);
+			$cat_params = json_decode($this->info_cat[0]->params);
+			$this->cat_img = "";
+			if ($cat_params->image != "") {
+			    $this->cat_img = '<img src="cid:catimg"  alt="'.$cat_params->image_alt.'" /> ';
+			}
+			$images  = json_decode($article->images);			
+			$article->introimg = ""; 
+			if (!empty($images->image_intro)) { // into img exists
+				$uneimage = '<img src="cid:introimg" alt="'.htmlspecialchars($images->image_intro_alt).'">';
+				$article->introimg =$uneimage; 
+			}
+			$article_tags = self::getArticleTags($article->id);
+	 		$this->itemtags = "";
+			foreach ($article_tags[$article->id] as $tag) {
+				$this->itemtags .= '<span class="iso_tag_'.$tag->alias.'">'.(($this->itemtags == "") ? $tag->tag : "<span class='iso_tagsep'><span>-</span></span>".$tag->tag).'</span>';
+            }
+            $this->needCatImg = false;
+            $this->needIntroImg = false;
+			$subject = $this->createSubject($creator,$article);
+			$body = $this->createBody($creator,$article);
 			foreach ($users as $user_id) {
 				// Load language for messaging
 				$receiver = Factory::getUser($user_id);
 				$go = false;
-				foreach ($receiver->groups as $group) { // controle des groupes
-					if (in_array($group,$usergroups)) {
-						$go = true;
-						break;
-					}
-				}
-				if (!$go) { // pas dans les groupes
-					continue;
-				}
 				$data = $receiver->getProperties();
 				$data['fromname'] = $config->get('fromname');
 				$data['mailfrom'] = $config->get('mailfrom');
@@ -121,18 +139,103 @@ class PlgContentPublishedArticle extends CMSPlugin
 
 				$lang = Factory::getLanguage();
 				$lang->load('plg_content_publishedarticle');
-				if (($user_id == $creatorId) && ($msgcreator = 1)) { // mail specifique au createur de l'article
-					$emailSubject = sprintf($lang->_('PLG_CONTENT_PUBLISHEDARTICLE_MSGCREATOR_SUBJECT'),$article->title);
-					$emailBody = sprintf($lang->_('PLG_CONTENT_PUBLISHEDARTICLE_MSGCREATOR_CONTENT'), $article->title,$url);
-						
+				if (($user_id == $creatorId) && ($msgcreator == 1)) { // mail specifique au createur de l'article
+					$emailSubject = $this->creatorSubject($creator,$article,"Article publié");
+					$emailBody = $this->creatorBody($creator,$article,"Votre article <b>{title}</b> est publié.<br>Vous pouvez le voir {url}");
 				} else 
 				{ // mail pour tous les autres
-					$emailSubject = sprintf($lang->_('PLG_CONTENT_PUBLISHEDARTICLE_MSG_SUBJECT'), $article->title);
-					$emailBody = sprintf($lang->_('PLG_CONTENT_PUBLISHEDARTICLE_MSG_CONTENT'), $creator->get('name'), $article->title, $url);
+					$emailSubject = $subject;
+					$emailBody = $body;
 				}
-				$return = Factory::getMailer()->sendMail($data['mailfrom'], $data['fromname'], $data['email'], $emailSubject, $emailBody, true);
+				$mailer = Factory::getMailer();
+				$config = Factory::getConfig();
+				$sender = array( 
+					$config->get( 'mailfrom' ),
+					$config->get( 'fromname' ) 
+				);
+				$mailer->setSender($sender);
+				$mailer->addRecipient($data['email']);
+				$mailer->setSubject($emailSubject);
+				$mailer->isHtml(true);
+				$mailer->Encoding = 'base64';
+				$mailer->setBody($emailBody);
+				if ((($msgcreator == 1) && ($user_id <> $creatorId)) || ($msgcreator == 0)) { // don't send images to creator
+				    if ($this->needCatImg)
+				        $mailer->AddEmbeddedImage(JPATH_ROOT.'/'.$cat_params->image,'catimg');
+			        if ($this->needIntroImg)
+			            $mailer->AddEmbeddedImage(JPATH_ROOT.'/'.$images->image_intro,'introimg');
+				}
+				$send = $mailer->Send();
 			}
 		}
 		return true;
+	}
+	private function createSubject($creator,$article) {
+		$libdateformat = "d/M/Y h:m";
+		$subject = $this->params->get('subject', "Nouvel article: {title}");
+		if (strpos($subject,'{catimg}')) $this->needCatImg = true;
+		if (strpos($subject,'{introimg}')) $this->needIntroImg = true;
+		$arr_css= array("{creator}"=>$creator->name,"{id}"=>$article->id,"{title}"=>$article->title, "{cat}"=>$this->info_cat[0]->title,"{date}"=>HTMLHelper::_('date', $article->created, $libdateformat), "{intro}" => $article->introtext, "{catimg}" => $this->cat_img, "{url}" => $this->url, "{introimg}"=>$article->introimg, "{subtitle}" => $article->subtitle, "{tags}" => $itemtags,"{featured}" => $article->featured); 
+		foreach ($arr_css as $key_c => $val_c) {
+			$subject = str_replace($key_c, Text::_($val_c),$subject);
+		}
+		return $subject;
+	}
+	private function creatorSubject($creator,$article,$msg) {
+		$libdateformat = "d/M/Y h:m";
+		$subject = $msg;
+		if (strpos($subject,'{catimg}')) $this->needCatImg = true;
+		if (strpos($subject,'{introimg}')) $this->needIntroImg = true;
+		$arr_css= array("{creator}"=>$creator->name,"{id}"=>$article->id,"{title}"=>$article->title, "{cat}"=>$this->info_cat[0]->title,"{date}"=>HTMLHelper::_('date', $article->created, $libdateformat), "{intro}" => $article->introtext, "{catimg}" => $this->cat_img, "{url}" => $this->url, "{introimg}"=>$article->introimg, "{subtitle}" => $article->subtitle, "{tags}" => $itemtags,"{featured}" => $article->featured); 
+		foreach ($arr_css as $key_c => $val_c) {
+				$subject = str_replace($key_c, Text::_($val_c),$subject);
+		}
+		return $subject;
+	}
+	private function createBody($creator,$article) {
+		$libdateformat = "d/M/Y h:m";
+		$body = $this->params->get('body', "Publication d'un article par : {creator}, titre : {title}.<br/>Il est visible {url}");
+		if (strpos($body,'{catimg}')) $this->needCatImg = true;
+		if (strpos($body,'{introimg}')) $this->needIntroImg = true;
+		$arr_css= array("{creator}"=>$creator->name,"{id}"=>$article->id,"{title}"=>$article->title, "{cat}"=>$this->info_cat[0]->title,"{date}"=>HTMLHelper::_('date', $article->created, $libdateformat), "{intro}" => $article->introtext, "{catimg}" => $this->cat_img, "{url}" => $this->url, "{introimg}"=>$article->introimg, "{subtitle}" => $article->subtitle, "{tags}" => $itemtags,"{featured}" => $article->featured); 
+		foreach ($arr_css as $key_c => $val_c) {
+				$body = str_replace($key_c, Text::_($val_c),$body);
+		}
+		return $body;
+	}
+	private function creatorBody($creator,$article,$msg) {
+		$libdateformat = "d/M/Y h:m";
+		$body = $msg;
+		if (strpos($body,'{catimg}')) $this->needCatImg = true;
+		if (strpos($body,'{introimg}')) $this->needIntroImg = true;
+		$arr_css= array("{creator}"=>$creator->name,"{id}"=>$article->id,"{title}"=>$article->title, "{cat}"=>$this->info_cat[0]->title,"{date}"=>HTMLHelper::_('date', $article->created, $libdateformat), "{intro}" => $article->introtext, "{catimg}" => $this->cat_img, "{url}" => $this->url, "{introimg}"=>$article->introimg, "{subtitle}" => $article->subtitle, "{tags}" => $itemtags,"{featured}" => $article->featured); 
+		foreach ($arr_css as $key_c => $val_c) {
+				$body = str_replace($key_c, Text::_($val_c),$body);
+		}
+		return $body;
+	}
+    private function getCategoryName($id) 
+	{
+		$db = Factory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select('*')
+			->from('#__categories ')
+			->where('id = '.(int)$id)
+			;
+		$db->setQuery($query);
+		return $db->loadObjectList();
+	}
+	private function getArticleTags($id) {
+		$db = Factory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select('tags.title as tag, tags.alias as alias, tags.note as note, tags.images as images, parent.title as parent_title, parent.alias as parent_alias')
+			->from('#__contentitem_tag_map as map ')
+			->innerJoin('#__content as c on c.id = map.content_item_id') 
+			->innerJoin('#__tags as tags on tags.id = map.tag_id')
+			->innerJoin('#__tags as parent on parent.id = tags.parent_id')
+			->where('c.id = '.(int)$id.' AND map.type_alias like "com_content%"')
+			;
+		$db->setQuery($query);
+		return $db->loadObjectList();
 	}
 }
